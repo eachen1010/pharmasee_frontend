c2f0d3d8f93efebfbe1b12dccc8e040e4dc38ebe
Alyssia Tan <aly.tan@outlook.com>
2024-01-27 19:31:11 -0800

    added member name top right corner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <nav style={{ backgroundColor: '#44ACCF', display: 'flex', justifyContent:'space-between', }} className="navbar">
-      <ul style={{ paddingLeft: '1vw', paddingBottom: '0.5vw', paddingTop: '0.5vw'}}>
+      <ul style={{ paddingLeft: '1vw', paddingBottom: '0.5vw', paddingTop: '1vw'}}>
         <li style={{ backgroundColor: '#44ACCF', color: 'white', display: 'inline', alignItems: 'center' }}>
           <img onClick={handleClick} src={ NavBarLogo } alt="Home" width="200px" className="iconStyle" />
         </li>
@@ -27,7 +27,7 @@ const Navbar = () => {
         <label style={{fontSize: '20px', width: '100%', padding: '1vw',}}> 
           TheMillers
         </label>
-        <img src={avatar} alt='avatar' width = 'auto' height='10px'/> 
+        <img src={avatar} alt='avatar' style={{marginTop: '.8vh', marginRight: '2vh', height:'45px'}}/> 
       </div>
     </nav>
   );
