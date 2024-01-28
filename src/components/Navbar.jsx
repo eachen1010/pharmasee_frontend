c5dbcbe3d47b3dff7d3875916d7a69de02a41a30
Alyssia Tan <aly.tan@outlook.com>
2024-01-28 10:17:04 -0800

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,18 +34,18 @@ const Navbar = () => {
     @import url('https://fonts.googleapis.com/css2?family=Istok+Web:ital,wght@0,400;0,700;1,400;1,700&display=swap')
     </style>
     <nav style={{ width: '100vw', height: '75px', backgroundColor: '#44ACCF', display: 'flex', justifyContent:'space-between' }} className="navbar">
-      <ul style={{ paddingLeft: '1vw', paddingBottom: '0.5vw', paddingTop: '1vw'}}>
+      <ul style={{ paddingLeft: '1vw', paddingBottom: '0.5vw', paddingTop: '1.2vw'}}>
         <li style={{ backgroundColor: '#44ACCF', color: 'white', display: 'inline', alignItems: 'center' }}>
           <img onClick={handleClick} src={ NavBarLogo } alt="Home" width="200px" className="iconStyle" />
         </li>
       </ul>
       <div style={{ display:'flex', width: '15%', marginBottom: '1vh', borderRadius: '6px', color: 'white' }}>
         <Menu>
-          <MenuButton display='flex' justify-content='center' flex-direction='row' style={{ borderTopLeftRadius: '60px 60px', borderBottomLeftRadius: '60px 60px', fontSize: '20px', marginRight: '0px', width: '100%', height: '75px', backgroundColor: '#3892b0'}}> 
-            <p>TheMillers</p>
+          <MenuButton display='flex' justify-content='center' flex-direction='row' className="buttonStyle" style={{ borderTopLeftRadius: '60px 60px', borderBottomLeftRadius: '60px 60px', fontSize: '3.2vh', marginRight: '0px', width: '100%', height: '75px', opacity:'90%', borderWidth: '5px',  borderColor: '#82c9e1'}}> 
+            <strong>TheMillers &nbsp; ▼</strong>
           </MenuButton>
           <MenuList>
-            <MenuItem onClick={handleMenuClick} background='#747474'>
+            <MenuItem onClick={handleMenuClick} background='#a6a6a6'>
               <span color="black">Log Out</span>
             </MenuItem>
           </MenuList>
