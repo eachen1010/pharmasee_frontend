41a82d4697c6acf99ebd61fabcfa2f655999c625
Emmy Chen <emmyc3@uci.edu>
2024-01-27 22:07:37 -0800

    Committing again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Navbar.css';
 import NavBarLogo from './NavBarLogo.png';
 import { useNavigate } from 'react-router-dom';
+import { Button } from "@chakra-ui/react";
 import avatar from './Avatar.png';
 
 const Navbar = () => {
@@ -13,7 +14,7 @@ const Navbar = () => {
       navigate('/dashboard');
       
     } catch (e) {
-      console.log(e);//setError('Failed to log in');
+      console.log(e); //setError('Failed to log in');
     }
   };
 
@@ -25,9 +26,9 @@ const Navbar = () => {
         </li>
       </ul>
       <div style={{display:'flex',marginRight: '1vh',width: '15%', marginBottom: '1vh', marginTop: '1vh',borderRadius: '6px', color: 'white', backgroundColor: '#3892b0'}}>
-        <label style={{fontSize: '20px', width: '100%', padding: '1vw',}}> 
+        <Button style={{fontSize: '20px', width: '100%', padding: '1vw',}}> 
           TheMillers
-        </label>
+        </Button>
         <img src={avatar} alt='avatar' style={{marginTop: '.8vh', marginRight: '2vh', height:'45px'}}/> 
       </div>
     </nav>
